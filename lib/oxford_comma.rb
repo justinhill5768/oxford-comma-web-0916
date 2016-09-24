e5088a521572add51f6d64baff38456cc1a96642
Justin Hill <justinhill5768@gmail.com>
2016-09-24 19:23:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+elsif array.length >= 3
+output = array[0..(array.length - 2)].join(", ")
+output = output + ", and #{array[-1]}"
+return output
+end
 end
